7cf4ef1865c589d821931561a67c3fdd17840199
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2019-02-28 15:36:29 -0500

    Bio changes Bottom ®

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -133,7 +133,9 @@ const Index = () => (
           </ContactText>
         </Inner>
         <Footer>
-          &copy; Daniello Consulting Inc. 2019 ® 
+          &copy; Daniello Consulting Inc. 2019 ® {' '}
+          <a href=""></a>. {' '}
+          <a href=""></a>.
         </Footer>
       </Contact>
     </Parallax>
