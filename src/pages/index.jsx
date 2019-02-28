2861a47f46ba9cf1576015eb29d6b7fed6db13fe
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2019-02-28 13:59:00 -0500

    <Subtitle> We are creating noice web experiences, desgins and strategic plans for the next generation of consumer-facing companies.</Subtitle>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -65,7 +65,7 @@ const Index = () => (
         <BigTitle>
           Hello, <br /> We're <br /> Daniello Consulting.
         </BigTitle>
-        <Subtitle> We are creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle> We are creating noice web experiences, desgins and strategic plans for the next generation of consumer-facing companies.</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
